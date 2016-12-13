d346fa298ae782be9b491a3a457d97538f8ef795
Tobe O <thosakwe@gmail.com>
2016-12-13 08:18:07 -0500

    Update core_plugins_test.dart

--- test/plugins/core_plugins_test.dart
+++ test/plugins/core_plugins_test.dart
@@ -2,7 +2,7 @@ import 'package:angel_roadmap/angel_roadmap.dart';
 import 'package:test/test.dart';
 
 main() {
-  test('all 13 core plugins pass', () {
+  test('all 13 core plugins pass', () async {
     await ensureAllReposPass(CORE_PLUGINS);
   });
 }
