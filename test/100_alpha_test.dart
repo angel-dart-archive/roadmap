01ebb805439821084cee8143403d1d2fa2f88697
Tobe O <thosakwe@gmail.com>
2016-12-18 20:53:19 -0500

    Update and rename 100_beta_test.dart to 100_alpha_test.dart

--- test/100_alpha_test.dart
+++ test/100_alpha_test.dart
@@ -2,8 +2,8 @@ import 'package:angel_roadmap/angel_roadmap.dart';
 import 'package:test/test.dart';
 
 main() {
-  group('1.0.0-beta', () {
+  group('1.0.0-alpha', () {
     test('Core API', () => ensureAllReposPass(CORE_API));
-    test('Plugins', () => ensureAllReposPass(BETA_PLUGINS));
+    test('Plugins', () => ensureAllReposPass(ALPHA_PLUGINS));
   });
 }
